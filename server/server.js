5e4798053694276d3fcbcdbee47cd47abb4aa984
Mohit Thakur <mt3046647@gmail.com>
2026-07-04 15:45:50 +0530

    change in server.js

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Handle unhandled promise rejections
